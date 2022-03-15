9ef249dd1592aa2cd149103b6508a5d55dee2e4f
stanislaviskandarov <101622311+stanislaviskandarov@users.noreply.github.com>
2022-03-15 15:21:46 +0600

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto">
+    <h1 class="text-6xl text-center">{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
